6089f2f2594fa140ed5e59da60b62b3ce28d1226
Gordon <gordon.shotwell@gmail.com>
2015-10-05 13:36:35 -0300

    initial commit

---
+++ R/easy_make.R
@@ -0,0 +1,95 @@
+#' easy_make
+#' @description Produce a makefile based on
+#'
+#' @param dependencies
+#' A dataframe containing the edge list of dependencies
+#' between files. The dataframe should have two fields "file" and "pre_req"
+#' showing the file and its immediate pre-requisite respectively. If a file
+#' has multiple #' pre-requisites, list each dependency as a separate line
+#' in the dataframe.
+#' @param render_markdown
+#' If TRUE, the Makefile will render any Markdown files, if FALSE markdown
+#' files will be ignored.
+#' @param path
+#' Destination path of the Makefile.
+#'
+#' @return
+#' A Makefile
+#' @export
+#' @importFrom dplyr group_by summarise filter left_join %>%
+#'
+#' @examples
+#' dependencies <- data_frame(
+#' file    = c("analysis/file2.R", "analysis/markdown.Rmd", "mtcars.csv",
+#' 						"mtcars.RData", "analysis/file2.R"),
+#' pre_req = c("mtcars.csv", "mtcars.RData", "analysis/file1.R",
+#' 					"analysis/file2.R", "R/hello.R"))
+#' easy_make(dependencies)
+#'
+easy_make <- function(dependencies,
+											 render_markdown = TRUE,
+											 path = "Makefile"){
+	dependencies$file_type    <- tools::file_ext(dependencies$file)
+	dependencies$pre_req_type <- tools::file_ext(dependencies$pre_req)
+
+		all_dependenciesendencies <- dependencies %>%
+		group_by(file) %>%
+		summarise(pre_req = paste(pre_req, collapse = " "),
+							file_type = file_type[1],
+							pre_req_type = ifelse(
+								(n() == 1) | all(pre_req_type == "R"),
+								pre_req_type[1], "multiple"))
+
+	r_dependenciesendencies <- dependencies %>%
+		filter(pre_req_type == "R") %>%
+		group_by(file) %>%
+		summarise(R_pre_req = paste(
+			paste("\tRScript", pre_req),
+			collapse = "\r"))
+
+	dependencies <- left_join(all_dependenciesendencies, r_dependenciesendencies, by = "file")
+	make <- rep("", length.out = length(dependencies$file))
+
+for (i in seq_along(dependencies$file)) {
+	# If target is R, run the target only,
+	# If all dependenciesendencies are R, run each dependenciesendency in order
+	# If all files are R, run each dependenciesendency in order, then run the target
+	if (dependencies$file_type[i] == "R" & dependencies$pre_req_type[i] != "R") {
+		make[i] <- paste0(dependencies$file[i], ": ", dependencies$pre_req[i],
+											"\r",
+											"\tRScript ", dependencies$file[i],
+											"\r ")
+	} else if (dependencies$file_type[i] != "R" & dependencies$pre_req_type[i] == "R") {
+		make[i] <- paste0(dependencies$file[i], ": ", dependencies$pre_req[i],
+											"\r",
+											"\tRScript ", dependencies$pre_req[i],
+											"\r ")
+	} else if (dependencies$file_type[i] == "R" & dependencies$pre_req_type[i] == "R") {
+		make[i] <- paste0(dependencies$file[i], ": ", dependencies$pre_req[i],
+											"\r",
+										   dependencies$R_pre_req[i],
+											"\r ",
+											"\tRScript ", dependencies$file[i],
+											"\r",
+											"\r")
+	} else if (dependencies$file_type[i] == "Rmd" & render_markdown) {
+		make[i] <- paste0(dependencies$file[i], ": ", dependencies$pre_req[i],
+											"\r",
+											"\tRscript -e 'rmarkdown::render(",
+											dependencies$file[i], ")'",
+											"\r ")
+	} else if (dependencies$pre_req_type[i] == "Rmd" & render_markdown) {
+		make[i] <- paste0(dependencies$file[i], ": ", dependencies$pre_req[i],
+											"\r",
+											"\tRscript -e 'rmarkdown::render(",
+											dependencies$pre_req[i], ")'",
+											"\r ")
+	}	else {
+		next
+	}
+}
+fileConn <- file(path)
+writeLines(make,  fileConn)
+close(fileConn)
+}
+
---
+++ R/graph_dependencies.R
@@ -0,0 +1,51 @@
+#' graph_dependencies
+#'
+#' @param files A character vector of filenames within the project,
+#' for instance one produced by list.files(recursive = TRUE)
+#' @param dependencies A dataframe containing the edge list of dependencies
+#' between files. This is the same file which is passed to easy_make(). The
+#' dataframe should have two fields "file" and "pre_req" showing the file
+#' and its immediate pre-requisite respectively. If a file has multiple
+#' pre-requisites, list each dependency as a separate line in the dataframe.
+#'
+#' @return A DiagrammeR plot showing the dependency graph.
+#' @export
+#'
+#' @examples
+#'
+#' filelist <-  c("analysis/file1.R", "analysis/file2.R", "analysis/markdown.Rmd",
+#' "dep.RData", "Makefile", "maker.Rproj", "man/hello.Rd",
+#' "mtcars.csv", "mtcars.RData", "output.txt", "R/dep_graph.R",
+#' "R/hello.R", "R/make_maker.R")
+#'
+#' dependencies <- data_frame(
+#' file    = c("analysis/file2.R", "analysis/markdown.Rmd", "mtcars.csv",
+#' 						"mtcars.RData", "analysis/file2.R"),
+#' pre_req = c("mtcars.csv", "mtcars.RData", "analysis/file1.R",
+#' 					"analysis/file2.R", "R/hello.R"))
+#' graph_dependencies(filelist, dependencies)
+#'
+graph_dependencies <- function(files, dependencies){
+
+	file_df <- data.frame(files, stringsAsFactors = FALSE)
+	file_df$type <- tools::file_ext(file_df$files)
+	file_df$shape <- ifelse(file_df$type %in% c("R", "Rmd"), "circle", "square")
+
+	nodes <-
+		DiagrammeR::create_nodes(nodes = file_df$files,
+														 shape = file_df$shape)
+
+	edges <-
+		DiagrammeR::create_edges(from = dependencies$pre_req,
+														 to = dependencies$file,
+														 relationship = "leading to")
+
+	graph <-
+		DiagrammeR::create_graph(nodes_df = nodes,
+														 edges_df = edges,
+														 graph_attrs = "layout = circo")
+
+	DiagrammeR::render_graph(graph)
+}
+
+
